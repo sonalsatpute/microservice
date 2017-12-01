d90feb9916db19bbe54b60df8687e00d36a54b41
sonalsatpute <sonal.satpute@gmail.com>
2017-12-01 20:39:03 +0530

    added rabbitmq consumer

---
+++ src/MessageBroker/Consumer.cs
@@ -0,0 +1,31 @@
+﻿using EasyNetQ;
+using EasyNetQ.Topology;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageBroker
+{
+  public class Consumer
+  {
+    IBus _bus;
+
+    public Consumer()
+    {
+      _bus = RabbitHutch.CreateBus(@"host=localhost;virtualHost=/;username=sonal;password=password");
+    }
+
+    public void Start(string queueName)
+    {
+      Console.WriteLine("Start method");
+
+      IQueue queue = new Queue(queueName, false);
+      _bus.Advanced.Consume(queue, (body, properties, info) => Task.Factory.StartNew(() =>
+      {
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(message);
+      }));
+    }
+  }
+
+}
---
+++ src/Service/Program.cs
@@ -0,0 +1,27 @@
+﻿using MessageBroker;
+using System;
+using System.Threading;
+
+namespace Service
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.ReadKey();
+    }
+
+    private static void Service()
+    {
+      Console.WriteLine("");
+    }
+
+    static void Consumer()
+    {
+      Consumer consummer = new Consumer();
+      consummer.Start("test-queue");
+
+      Console.WriteLine("Running other Thread");
+    }
+  }
+}
